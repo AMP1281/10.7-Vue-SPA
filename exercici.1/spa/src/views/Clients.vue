5286e9c9de16cc93b598f221a2d4359b7a44e954
Anna Montserrat <amp1281@gmail.com>
2021-07-23 17:24:15 +0200

    ok

--- exercici.1/spa/src/views/Clients.vue
+++ exercici.1/spa/src/views/Clients.vue
@@ -5,24 +5,29 @@
     <b-container>
 
     <h1>Clients</h1>
+
+          <ul class="row list-unstyled">
+
+              <li class="col-md-4 mt-4 d-flex align-items-stretch justify-content-center" 
+              v-for="(item, index) in clientesArreglo" :key="index">
+
+                  <div class="card text-center w-100">
+                    <h5 class="card-header py-4">
+                      {{item.nomClient}}</h5>
+
+                    <div class="card-body py-5">
+
+                      <p class="card-text"><b-button @click="mostrarDetall()">Detalls</b-button></p>
+
+                    </div>
+
+                  </div>
+
+              </li>
+
+          </ul>
         
-        <!--con : delante de to pasamos una variable-->
-        <router-link :to="{name:'Clients', params:{id: item}}"
-
-          v-for="(item, index) in clientesArreglo" :key="index">
-
-
-
-          <li><button>Client: {{item}}</button></li>
-
-        </router-link>
-
-
-        <ClienteComp/> <!--importamos ClienteComp-->
-
-    <!--     <button @click="home">Home</button>
-    <button @click="anterior">Anterior</button>
-    <button @click="siguiente">Siguiente</button> -->
+    <ClienteComp/>
 
     </b-container>
 
@@ -36,15 +41,37 @@ import ClienteComp from '@/components/ClienteComp.vue'
 
 export default {
   name:'Clients',
+  data(){
+    return {
+      clientesArreglo: [
+      {
+        nomClient:"Carme",
+        mailClient:"carme@email.com",
+        telClient: 658111111,
+        idClient: 1
+      },
+      {
+        nomClient:"Rosa",
+        mailClient:"rosa@email.com",
+        telClient: 658222222,
+        idClient: 2
+      },
+      {
+        nomClient:"Anna",
+        mailClient:"anna@email.com",
+        telClient: 658333333,
+        idClient: 3
+      }
+      ]
+    }
+  },
   components: {
     ClienteComp
   },
-  data(){
-    return {
-      clientesArreglo: ["Carme","Rosa", "Anna"]
-    }
-  },
   methods:{
+    mostrarDetall(){
+      this.$router.push(name:'details')
+    },
     home(){
       this.$router.push('/')
     },
@@ -54,6 +81,6 @@ export default {
     siguiente(){
       this.$router.go(1)
     }
-  }
+  },
 }
 </script>
