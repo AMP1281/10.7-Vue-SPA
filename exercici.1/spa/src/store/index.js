327d819aafe8463e97f080aee2a083bb09bedfad
Anna Montserrat <amp1281@gmail.com>
2021-07-28 15:13:43 +0200

    ok

--- exercici.1/spa/src/store/index.js
+++ exercici.1/spa/src/store/index.js
@@ -2,45 +2,60 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import axios from 'axios'
 
-
 Vue.use(Vuex)
 
 export default new Vuex.Store({
+
   state: {
     usuariosArreglo:[],
     picturesArreglo:[],
+    numeroUsers: 0,
+    param:0
   },
+  
+  //Obtenemos una copia del state, no modifica
   getters:{
+
+    //Devuelve usuario con id que indiquemos
     getCliente: (state) => (id) => {
       return state.usuariosArreglo.find(usuario => usuario.id === id)
     },
-    getPicture: (state) => (id) => {
-      return state.picturesArreglo.find(picture => picture.albumId === id)
-    },
-    getDuplicated: state =>{
+
+    //Devuelve un array sin albumId duplicado
+    getDuplicated: state => {
       return state.picturesArreglo.filter((album, index, self) => index === self.findIndex((obj)=>(obj.albumId === album.albumId)))},
+
   },
 
   mutations: {
+
     SET_USERS(state, users){
        state.usuariosArreglo = users
      },
+
     SET_PICTURES(state, pictures){
       state.picturesArreglo = pictures
     },
 
+    aumentar(state, paramID){
+      state.numeroUsers++ ;
+      state.param = paramID
+    }
 
             /*llenarUsuarios(state,usuariosAccion){
                 state.usuariosArreglo = usuariosAccion //llena el array
             } */
   },
+
   actions: {
+
     getUsers({ commit }) {
       axios.get('http://jsonplaceholder.typicode.com/users')
           .then(response => {
               commit('SET_USERS', response.data)
           })
     },
+
     getPictures({ commit }) {
       axios.get('http://jsonplaceholder.typicode.com/photos')
           .then(response => {
@@ -48,9 +63,12 @@ export default new Vuex.Store({
           })
     } 
   },
+
+          //Llamar con async-await
           /*getUsers: async function({commit}){
               const data = await fetch ('http://jsonplaceholder.typicode.com/users'); //llamamos a la api
               const usuarios = await data.json();//guardamos en la const usuarios
               commit('llenarUsuarios', usuarios)
           }*/
+
 })
