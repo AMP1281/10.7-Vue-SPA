c5cb159aaa416452623a5dfbc8432b34642b5289
Anna Montserrat <amp1281@gmail.com>
2021-07-30 19:52:15 +0200

    ok

---
+++ exercici.1/spa/src/components/Autcomplete.vue
@@ -0,0 +1,46 @@
+<template>
+
+<div>
+
+        <b-nav-item to= "/Users/:id" >
+
+            <b-form-input type="text" v-model="userInput" list="resultAutocomplete" placeholder="Username search.."></b-form-input>
+
+            <datalist id="resultAutocomplete">
+
+                <option v-for="name in resultAutocomplete" :key="name">{{ name.name }}</option>
+
+            </datalist>
+
+        </b-nav-item>
+ 
+</div>
+
+</template>
+
+<script>
+
+import {mapGetters} from 'vuex'
+
+export default{
+
+    computed:{
+
+         ...mapGetters(['resultAutocomplete']),
+
+         userInput:{
+
+              get(){
+                  return this.$store.state.inputBuscador
+              },
+
+              set(userInput){
+                  return this.$store.commit('SET_BUSCADOR', userInput)
+              }
+
+        },
+    },
+}
+
+
+</script>
